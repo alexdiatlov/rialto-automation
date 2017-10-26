579a31d495ab5ed117f12e08c68fb6a08c9ed49f
alexdiatlov <30532587+alexdiatlov@users.noreply.github.com>
2017-10-26 11:10:11 +0300

    clear category test
    clear category test for refill

--- src/test/java/ua/com/rialtotenders/tests/CategorySearchTest.java
+++ src/test/java/ua/com/rialtotenders/tests/CategorySearchTest.java
@@ -1,11 +1,10 @@
 package ua.com.rialtotenders.tests;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import ua.com.rialtotenders.pageObjects.MainPage;
+import org.openqa.selenium.support.ui.Select;
 import ua.com.rialtotenders.pageObjects.SearchPage;
 
 import java.util.Locale;
@@ -13,7 +12,6 @@ import java.util.Locale;
 public class CategorySearchTest extends BaseTest {
 
     private SearchPage searchPage;
-    private MainPage mainPage;
 
     @Before
     public void setUp() {
@@ -21,138 +19,21 @@ public class CategorySearchTest extends BaseTest {
     }
 
     @Test
-    public void office_category_tender_test() throws InterruptedException {
-
-        mainPage = new MainPage(getDriver());
+    public void office_category_test() throws InterruptedException {
+        searchPage = new SearchPage(getDriver());
         //LandingPage landingPage= new LandingPage(getDriver());
         //landingPage.clickTenderSearch();
-        mainPage.clickTenderSearch();
+        searchPage.clickTenderSearch();
 
         searchPage = new SearchPage(getDriver());
         searchPage.clickClearFilter();
 
-        searchPage.getCategorySelectBox().selectByVisibleText("Все для офісу");
-        Thread.sleep(2000);
+        searchPage.selectCategoryByText("Все для офісу");
+        //Thread.sleep(2000);
         searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct", driver.findElement(By.cssSelector(".sb-table__text--small")).getText().contains("30000000-9"));
 
        /* WebElement category = driver.findElement(By.cssSelector(".select-category > select"));
         Select select = new Select(category);
         select.selectByVisibleText("Все для офісу");*/
     }
-
-    @Test
-    public void computer_category_tender_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickTenderSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.clickClearFilter();
-
-        searchPage.getCategorySelectBox().selectByVisibleText("Комп'ютери та програми");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct", driver.findElement(By.cssSelector(".sb-table__text--small")).getText().contains("30200000-1"));
-    }
-
-    @Test
-    public void products_tender_category() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickTenderSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.clickClearFilter();
-
-        searchPage.getCategorySelectBox().selectByVisibleText("Продукти харчування");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct", driver.findElement(By.cssSelector(".sb-table__text--small")).getText().contains("15000000-8"));
-    }
-
-    @Test
-    public void agro_products_tender_category_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickTenderSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.clickClearFilter();
-
-        searchPage.getCategorySelectBox().selectByVisibleText("Сільськогосподарська продукція");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct", driver.findElement(By.cssSelector(".sb-table__text--small")).getText().contains("15000000-8"));
-    }
-
-    @Test
-    public void agro_technic_tender_category_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickTenderSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.clickClearFilter();
-
-        searchPage.getCategorySelectBox().selectByVisibleText("Сільськогосподарська техніка");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct", driver.findElement(By.cssSelector(".sb-table__text--small")).getText().contains("16700000-2"));
-    }
-
-    @Test
-    public void office_category_plan_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickPlanSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.getCategorySelectBox().selectByVisibleText("Все для офісу");
-Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct",driver.findElement(By.cssSelector(".sb-table__table-row--pt0")).getText().contains("39260000-2"));
-    }
-
-    @Test
-    public void computers_category_plan_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickPlanSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.getCategorySelectBox().selectByVisibleText("Комп'ютери та програми");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct",driver.findElement(By.cssSelector(".sb-table__table-row--pt0")).getText().contains("32551300-3"));
-    }
-
-    @Test
-    public void products_category_plan_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickPlanSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.getCategorySelectBox().selectByVisibleText("Продукти харчування");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct",driver.findElement(By.cssSelector(".sb-table__table-row--pt0")).getText().contains("15000000-8"));
-    }
-
-    @Test
-    public void agro_products_category_plan_test() throws InterruptedException {
-        mainPage = new MainPage(getDriver());
-        mainPage.clickPlanSearch();
-
-        searchPage = new SearchPage(getDriver());
-        searchPage.getCategorySelectBox().selectByVisibleText("Продукти харчування");
-        Thread.sleep(2000);
-        searchPage.clickNameTrendByOrderNumber(0);
-
-        Assert.assertTrue("CPV code is not correct",driver.findElement(By.cssSelector(".sb-table__table-row--pt0")).getText().contains("15000000-8"));
-    }
-
-
 }
